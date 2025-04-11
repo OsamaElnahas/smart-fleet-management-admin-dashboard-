d987ca8d0cfc4379c4217070aaa9751dcecfd218
ziad10100 <ziad.mahmoud151@gmail.com>
2025-04-12 01:09:47 +0200

    add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 export default function App() {
-
-  return  <>
-    <h1 className='text-3xl font-bold underline'>Hello World osama</h1>
-    <h1 className='text-3xl font-bold underline'>Hello World zaiad</h1>
-    <h1 className='text-3xl font-bold underline'>Hello World mohamed</h1>
-      </>
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello World osama</h1>
+      <h1 className="text-3xl font-bold underline">Hello World zaiad</h1>
+      <h1 className="text-3xl font-bold underline">Hello World mohamed</h1>
+      <button className="rounded">Ziad Gamed</button>
+    </>
+  );
 }
