fc4ea3bf1ae8aa8d888095168e59692906f13e89
ziad10100 <ziad.mahmoud151@gmail.com>
2025-04-12 01:36:45 +0200

    tailwind installtion

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
-
 import "./App.css";
 
 export default function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello World osama</h1>
-      <h1 className="text-3xl font-bold underline">Hello World zaiad</h1>
-      <h1 className="text-3xl font-bold underline">Hello World mohamed</h1>
-      <button className="rounded">Ziad Gamed</button>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-gray-800 mb-4">
+        Hello World osama
+      </h1>
+      <h1 className="text-3xl font-bold text-gray-800 mb-4">
+        Hello World zaiad
+      </h1>
+      <h1 className="text-3xl font-bold text-gray-800 mb-4">
+        Hello World mohamed
+      </h1>
+      <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105">
+        Click me
+      </button>
+    </div>
   );
 }
