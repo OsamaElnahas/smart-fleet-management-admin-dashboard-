c87080eb83dd1a9f648d172f79217fea9c3ce735
ziad10100 <ziad.mahmoud151@gmail.com>
2025-04-12 02:00:23 +0200

    bl3b

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
-import "./App.css";
+import Dashboard from "./components/dashboard/dashboard";
+import Sidebar from "./components/sidebar/sidebar";
 
 export default function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold text-gray-800 mb-4">
-        Hello World osama
-      </h1>
-      <h1 className="text-3xl font-bold text-gray-800 mb-4">
-        Hello World zaiad
-      </h1>
-      <h1 className="text-3xl font-bold text-gray-800 mb-4">
-        Hello World mohamed
-      </h1>
-      <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105">
-        Click me
-      </button>
+    <div className="grid grid-cols-[200px,_1fr] gap-3">
+      <Sidebar />
+      <Dashboard />
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='bg-black min-h-[100vh] rounded '>
+        <h3 className='text-orange-500 m-10'>Omar Alarby</h3>
+    </div>
+  )
+}
+
+export default Sidebar
