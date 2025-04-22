03c6e6b7f0fb0735ef5e3d8dab3632ebe9f1b6fc
ziad10100 <ziad.mahmoud151@gmail.com>
2025-04-22 04:27:19 +0200

    d

--- src/components/AllUsersTable/AllUsersTable.jsx
+++ src/components/AllUsersTable/AllUsersTable.jsx
@@ -3,53 +3,37 @@ import { IoEllipsisVertical } from "react-icons/io5";
 
 export default function AllUsersTable() {
   return (
-    <div className="font-Inter grid grid-cols-[10%,20%,20%,20%,20%,10%] text-[1rem] font-[540] w-full gap-y-5">
-      {/* Head */}
-      <div className="text-[#04103B] p-3 rounded-lg font-[550] text-[1rem]">
-        ID
+    <div className="font-Inter flex flex-col gap-y-4 text-[1rem] font-[540] w-full ">
+      {/* HEAD */}
+      <div className="grid grid-cols-[10%,20%,20%,20%,20%,10%] text-[#04103B] bg-gray-100 p-3 rounded-lg font-[550]">
+        <div>ID</div>
+        <div>Driver Name</div>
+        <div>Driver Number</div>
+        <div>Driver Cos</div>
+        <div>Rating</div>
+        <div></div>
       </div>
-      <div className="text-[#04103B] p-3 rounded-lg font-[550] text-[1rem]">
-        Driver Name
+      {/* ROW */}
+      <div className="grid grid-cols-[10%,20%,20%,20%,20%,10%] bg-white p-4 rounded-xl shadow-md">
+        <div>1</div>
+        <div>Osama Elnahas</div>
+        <div>01005505505</div>
+        <div>218452</div>
+        <div>218452</div>
+        <button>
+          <IoEllipsisVertical className="text-[1.5rem]" />
+        </button>
       </div>
-      <div className="text-[#04103B] p-3 rounded-lg font-[550] text-[1rem]">
-        Driver Number
-      </div>
-      <div className="text-[#04103B] p-3 rounded-lg font-[550] text-[1rem]">
-        Driver Cos
-      </div>
-      <div className="text-[#04103B] p-3 rounded-lg font-[550] text-[1rem]">
-        Rating
-      </div>
-      <div className="text-[#04103B] p-3 rounded-lg font-[550] text-[1rem]"></div>
-
-      {/* Row 1 */}
-      <div className="bg-white p-4 rounded-tl-xl rounded-bl-xl">1</div>
-      <div className="bg-white p-4">Osama Elnahas</div>
-      <div className="bg-white p-4">01005505505</div>
-      <div className="bg-white p-4">218452</div>
-      <div className="bg-white p-4">218452</div>
-      <div className="bg-white p-4 rounded-tr-xl rounded-br-xl">
-        <IoEllipsisVertical />
+      <div className="grid grid-cols-[10%,20%,20%,20%,20%,10%] bg-white p-4 rounded-xl shadow-md">
+        <div>1</div>
+        <div>Osama Elnahas</div>
+        <div>01005505505</div>
+        <div>218452</div>
+        <div>218452</div>
+        <button>
+          <IoEllipsisVertical className="text-[1.5rem]" />
+        </button>
       </div>
     </div>
   );
 }
-// {/* <div className="font-Inter grid grid-cols-[10%,20%,20%,20%,20%,10%] text-[1rem] font-[540] w-full gap-y-5">
-//       {/* Head */}
-//       <div className="text-[#04103B] p-3 rounded-lg font-[550] text-[1rem]">ID</div>
-//       <div className="text-[#04103B] p-3 rounded-lg font-[550] text-[1rem]">Driver Name</div>
-//       <div className="text-[#04103B] p-3 rounded-lg font-[550] text-[1rem]">Driver Number</div>
-//       <div className="text-[#04103B] p-3 rounded-lg font-[550] text-[1rem]">Driver Cos</div>
-//       <div className="text-[#04103B] p-3 rounded-lg font-[550] text-[1rem]">Rating</div>
-//       <div className="text-[#04103B] p-3 rounded-lg font-[550] text-[1rem]"></div>
-
-//       {/* Row 1 */}
-//       <div className="bg-white p-4 rounded-tl-xl rounded-bl-xl">1</div>
-//       <div className="bg-white p-4">Osama Elnahas</div>
-//       <div className="bg-white p-4">01005505505</div>
-//       <div className="bg-white p-4">218452</div>
-//       <div className="bg-white p-4">218452</div>
-//       <div className="bg-white p-4 rounded-tr-xl rounded-br-xl">
-//         <IoEllipsisVertical />
-//       </div>
-//     </div> */}
